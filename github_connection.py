37ea30880d26aedb9bdd19685eb091c8d9fa35d4
daveondecks <daveondecks@gmail.com>
2025-03-02 18:11:23 +0000

    Add files via upload

--- github_connection.py
+++ github_connection.py
@@ -2,16 +2,15 @@ import streamlit as st
 import pandas as pd
 
 # Define the URL of the raw CSV file in the GitHub repository
-GITHUB_URL = "https://github.com/daveondecks/table_test.git/data_table/data.csv"
+GITHUB_URL = "https://raw.githubusercontent.com/daveondecks/table_test/main/data_table/data.csv"
 
-@st.experimental_connection
 def fetch_data():
     # Fetch the data from the GitHub URL
     data = pd.read_csv(GITHUB_URL)
     return data
 
 def main():
-    st.title("GitHub Data Connection Example")
+    st.title("GitHub Data Table Example")
 
     # Fetch the data
     data = fetch_data()
